71437f50c13c2a1543d3fdb19cac8d543eb5e56e
Sublustrum007ru <Sublustrum007@yandex.ru>
2024-10-14 01:41:19 +0300

    Update file Application.java

--- src/view/Application.java
+++ src/view/Application.java
@@ -1,7 +1,7 @@
 package view; 
 public class Application{ 
     public static void run(){ 
-        String helloMSG = "Hello World!!!\n My name is Sublustrum007"; 
+        String helloMSG = "Hello World!!!\nMy name is Sublustrum007";
         promt(helloMSG);  
     } 
     public static void promt(String str){ 
